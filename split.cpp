32e385a0f9c8b0260bab68432d43deeb76864743
Jason Byun <jsbyun@usc.edu>
2025-01-31 06:55:36 +0000

    all tests passed!

--- split.cpp
+++ split.cpp
@@ -13,11 +13,34 @@ the function below should be the only one in this file.
 #include "split.h"
 
 /* Add a prototype for a helper function here if you need */
+void goThrough(Node*& list, int value);
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  if (in == nullptr) { //base case!
+    return;
+  }
+  
+  Node* temp = in;
+  in = in->next;
+  
+  if (temp->value % 2 == 0){ //checks if even
+    goThrough(evens, temp->value);
+  }else{ //for odd ones
+   goThrough(odds, temp->value);
+  }
+
+  delete temp;
+  split(in, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
+void goThrough(Node*& list, int value){
+  if(list == nullptr){
+    list = new Node(value,nullptr);
+  } else if (list->next == nullptr) {
+    list->next = new Node(value, nullptr);
+  } else {
+    goThrough(list->next, value);
+  }
+}
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,66 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+  
+  Node* odds = nullptr;
+  Node* evens = nullptr;
+
+  Node* ins = new Node(0, nullptr);
+  Node* temp = ins;
+
+  for(int i = 1; i < 10; i++){
+    
+    Node* newNode = new Node(i, nullptr);
+    temp -> next = newNode;
+    temp = temp -> next;
+  }
+
+  Node* tempIns = ins;
+  cout<< "This is the stuff in Ins" << endl;
+  while(tempIns != nullptr){
+    cout << tempIns -> value << endl;
+    tempIns = tempIns -> next;
+  }
+
+  split(ins, odds, evens);
+
+  Node* tempOdd = odds;
+  cout<< "This is the stuff in Odds" << endl;
+  while(tempOdd != nullptr){
+    cout << tempOdd -> value << endl;
+    tempOdd = tempOdd -> next;
+  }
+
+  cout << endl;
+  cout << endl;
+
+  Node* tempEven = evens;
+  cout<< "This is the stuff in Even" << endl;
+  while(tempEven != nullptr){
+    cout << tempEven -> value << endl;
+    tempEven = tempEven -> next;
+  }
+
+  while (odds != nullptr) {
+    Node* temp = odds;
+    odds = odds->next;
+    delete temp;
+  }
+  
+  while (evens != nullptr) {
+    Node* temp = evens;
+    evens = evens->next;
+    delete temp;
+  }
+
+
+
+
+
 
 }
